3f4c4fce3e4ca0cb7a41565da9efa3e3ae2c669a
Saidul147 <sidul.islam.146@gmail.com>
2025-09-20 09:05:31 +0600

    project setup

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { AppConfig } from "@/config/app.config";
+
+export const metadata: Metadata = {
+  title: AppConfig().app.name,
+  description: AppConfig().app.slogan,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>{children}</body>
+    </html>
+  );
+}
